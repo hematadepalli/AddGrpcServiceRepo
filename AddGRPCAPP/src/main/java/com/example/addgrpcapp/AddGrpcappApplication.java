965830cdac3b0ef70dde94b974c75730ebff0811
htadepal <Hema03@ge>
2022-05-11 23:38:40 +0200

    Initial commit

---
+++ AddGRPCAPP/src/main/java/com/example/addgrpcapp/AddGrpcappApplication.java
@@ -0,0 +1,13 @@
+package com.example.addgrpcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddGrpcappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddGrpcappApplication.class, args);
+    }
+
+}
---
+++ AddGRPCAPP/src/test/java/com/example/addgrpcapp/AddGrpcappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addgrpcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddGrpcappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
